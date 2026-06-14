025043fe097cc0e57fecfc5173c5df9ce15f4f87
Ekta Singh Kushwaha <ektasinghkushwaha2@gmail.com>
2026-06-14 18:10:04 +0530

    Add files via upload

---
+++ day-15/Q60.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter number of elements:";
+    cin>>n;
+    int arr[n];
+    cout<<"enter array elements:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            arr[j]=arr[i];
+            j++;
+        }
+    }
+    while(j<n){
+        arr[j]=0;
+        j++;
+    }
+    cout<<"array after moving zereos:";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
